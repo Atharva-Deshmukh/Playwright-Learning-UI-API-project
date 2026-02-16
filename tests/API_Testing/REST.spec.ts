a3e4478f798ddf6cf16ae4741ef820f60a359474
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-02-16 21:46:44 +0530

    REST

--- tests/API_Testing/REST.spec.ts
+++ tests/API_Testing/REST.spec.ts
@@ -1,11 +1,59 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request, APIRequestContext } from '@playwright/test';
 
 /* API tests (like your REST API test) do not open a browser window, even in headed mode, 
 because they do not use the browser context or UI.
---headed only makes sense for tests that interact with a browser. */
+--headed only makes sense for tests that interact with a browser. 
 
-test('GET', async ({request}) => {
+NOTE:
+request is being fetched from two ways:
+
+Way-1 --> import { test, expect, request } from '@playwright/test';
+          the request we import has request.newContext() just like we have browserContext()
+
+Way-2 --> test('myTest', async ({request}) => {
+
+})
+
+*/
+
+
+/* Way-2: In below test, we use a readymade request fixture 
+   
+   This is:
+   - Automatically created and managed by Playwright
+   - Automatically cleaned up after the test
+   - No manual setup required
+*/
+test('GET - request Fixture', async ({request }) => {
     const response = await request.get('https://jsonplaceholder.typicode.com/posts');
+    await expect(response.status()).toBe(200);
+    const data = await response.json();
+    // await console.warn('data -> ', data);
+    await expect(data.length).toBeGreaterThan(5);
+});
+
+/* Way-1: imported request 
+
+- This allows you to manually create an API request context. 
+- Manual control over configuration
+- You must manually dispose it
+
+We use this when:
+- When creating multiple isolated contexts
+- We need custom configurations apart from test configurations
+
+Just like browser tests:
+    const browserContext = await browser.newContext();
+    const page = await browserContext.newPage();
+    page.locator()..
+
+    We created new browser context and then new page and then on this new page, we acted upon
+    Similarly, we create a new request context and we create a new request from this contexta and act upon
+*/
+test('GET - request import', async () => {
+    const requestContext: APIRequestContext = await request.newContext();
+    const response = await requestContext.get('https://jsonplaceholder.typicode.com/posts');
+
     await expect(response.status()).toBe(200);
     const data = await response.json();
     // await console.warn('data -> ', data);
@@ -21,7 +69,8 @@ test('POST', async ({request}) => {
         headers: {
             'x-api-key': 'reqres-free-v1',
             'Content-Type': 'application/json'
-        }
+        },
+        failOnStatusCode: false
     });
     await expect(await response.status()).toBe(201);
     const data = await response.json();
